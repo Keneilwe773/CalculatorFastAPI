860bb6b3f4088ef6ce0762b71b46703113b7fba7
keneilweraseale <keneilweboipelo.raseale@indsafri.com>
2026-07-24 14:08:50 +0200

    Save

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
@@ -34,4 +35,42 @@ def divide(num1 : float, num2 : float):
     else:
         return {
             "answer" : num1 / num2
+=======
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return FileResponse("index.html")
+
+@app.get("/add")
+def add(num1 : float, num2 : float):
+    return {
+        "answer" : num1 + num2
+    }
+
+@app.get("/subtract")
+def subtract(num1 : float, num2 : float):
+    return {
+        "answer" : num1 - num2
+    }
+
+@app.get("/multiply")
+def multiply(num1 : float, num2 : float):
+    return {
+        "answer" : num1 * num2
+    }
+
+@app.get("/divide")
+def divide(num1 : float, num2 : float):
+    if num2 == 0:
+        return {
+            "error" : "undefined"
+        }
+    else:
+        return {
+            "answer" : num1 / num2
+>>>>>>> 7b1b293 (Initial commit)
         }
